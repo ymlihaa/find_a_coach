d9bf6cad66055d5c94c6ea5b61aa8ca43e49f4d8
ymlihaa <ymlihaa@gmail.com>
2021-04-02 18:40:56 +0300

    register and login page refactoring .

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -55,9 +55,7 @@
 
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary" text @click="dialog = false">
-                  I accept
-                </v-btn>
+                <v-btn color="primary" text @click="dialog = false"> Ok </v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
