423e56b5dea4577f4bb2dbee0d586254d6b96038
ymlihaa <ymlihaa@gmail.com>
2021-04-12 16:11:53 +0300

    login page convert to elemen ui

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,49 +1,31 @@
 <template >
-  <transition name="fade">
-    <div class="w-100 d-flex-col">
-      <v-form
-        class="form w-50 d-flex-col"
-        ref="form"
-        v-model="valid"
-        lazy-validation
-        outlined
-        @submit.prevent="handleLogin"
-      >
-        <img src="../assets/login_logo.svg" alt="" />
-
-        <div class="w-50">
-          <v-text-field
-            class="w-100"
-            v-model="email"
-            :counter="10"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-
-          <v-text-field
-            class="input-group--focused w-100"
+  <el-container>
+    <el-main>
+      <el-form v-if="this.$store.state.dialog == false" align="center">
+        <el-form-item label="E-mail" prop="email">
+          <el-input v-model="email"></el-input>
+        </el-form-item>
+        <el-form-item label="Password" prop="pass">
+          <el-input
+            type="password"
             v-model="password"
-            :type="show3 ? 'text' : 'password'"
-            label="Password"
-            required
-          ></v-text-field>
-          <div class="d-flex-col">
-            <v-btn color="primary " class="w-100 mt-3" @click="handleLogin">
-              Login
-            </v-btn>
-            <v-btn color="error" class="w-100 mt-3" @click="reset">
-              Reset Form
-            </v-btn>
-          </div>
-        </div>
-      </v-form>
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleLogin">Log In</el-button>
+          <el-button type="danger" @click="reset">Reset</el-button>
+        </el-form-item>
+      </el-form>
       <dialog-app
         v-if="this.$store.state.dialog == true"
         :dialogMessage="errorMessage"
-      ></dialog-app></div
-  ></transition>
+      ></dialog-app>
+    </el-main>
+  </el-container>
 </template>
+
+
 
 
 <script>
@@ -59,8 +41,6 @@ export default {
     return {
       email: "",
       password: "",
-      valid: true,
-      show3: false,
       errorMessage: "",
       showDialog: this.$store.state.dialog,
     };
@@ -95,28 +75,13 @@ export default {
 </script>
 
 <style scoped>
-img {
-  width: 33%;
+.el-main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.form {
-  min-height: 70vh;
-}
-
-.mt-3 {
-  margin-top: 3rem;
-}
-.mt-10 {
-  margin-top: auto;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
+.el-form {
+  width: 80vmin;
 }
 </style>
