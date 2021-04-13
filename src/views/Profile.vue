89bd221cb396956b4636f0032220d6da510f87f8
ymlihaa <ymlihaa@gmail.com>
2021-04-13 16:08:46 +0300

    profile page converted to element UI ç

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,49 +1,34 @@
 <template>
-  <v-app id="inspire">
-    <v-main class="grey lighten-3">
-      <v-container>
-        <v-row>
-          <v-col>
-            <v-sheet min-height="70vh" rounded="lg">
-              <v-row>
-                <v-col>
-                  <v-btn elevation="2" small @click="editProfile"
-                    >Düzenle</v-btn
-                  >
+  <el-container>
+    <el-header>
+      <el-row :gutter="20">
+        <el-col :span="22">
+          {{ userDetail.firstName }} {{ userDetail.lastName }}
+        </el-col>
+        <el-col :span="2">
+          <el-button type="info" @click="editProfile">Edit</el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-divider></el-divider>
 
-                  <div class="p-2 d-flex-col">
-                    <v-avatar color="grey lighten-2" size="128"></v-avatar>
-                    <span class="title mt-1"
-                      >{{ userDetail.firstName }}
-                      {{ userDetail.lastName }}
-                    </span>
-
-                    <v-sheet min-height="30vh" rounded="lg">
-                      {{ userDetail.description }}
-                    </v-sheet>
-
-                    <v-card
-                      outlined
-                      class="p-half mr-1"
-                      v-for="(tag, index) in userDetail.tags"
-                      :key="index"
-                      max-width="72"
-                    >
-                      <v-list-item-subtitle class="mt-1"
-                        ><strong>{{ tag }}</strong></v-list-item-subtitle
-                      >
-                    </v-card>
-                  </div>
-
-                  <request-form v-if="isRequest == true"></request-form>
-                </v-col>
-              </v-row>
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+    <div class="content-wrapper">
+      <el-main type="flex">
+        <el-row :gutter="20">
+          <el-col>
+            <el-card>
+              {{ userDetail.description }}
+            </el-card>
+            <el-card>
+              <el-tag v-for="(tag, index) in userDetail.tags" :key="index">
+                {{ tag }}</el-tag
+              >
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-main>
+    </div>
+  </el-container>
 </template>
 
 <script>
@@ -81,26 +66,4 @@ export default {
 </script>
 
 <style scoped>
-.mt-5 {
-  margin-top: 5rem;
-}
-
-.detail-wrapper {
-  max-width: 720px;
-}
-
-.request {
-  float: right;
-  padding: 1rem;
-  border: 1px solid #cacaca;
-  border-radius: 8px;
-  margin: 1rem;
-  background-color: #488fef;
-  color: white;
-  cursor: pointer;
-}
-.p-half {
-  padding: 0.5rem;
-  width: auto;
-}
 </style>
