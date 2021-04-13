b2d325753c95413fd2973a6122f2688c13a87a7e
ymlihaa <ymlihaa@gmail.com>
2021-04-13 16:28:25 +0300

    edit Profile view converted to element UI .

--- src/views/editProfile.vue
+++ src/views/editProfile.vue
@@ -1,107 +1,39 @@
 <template>
-  <v-app id="inspire">
-    <v-main class="grey lighten-3">
-      <v-container>
-        <dialog-app
-          v-if="this.$store.state.dialog == true"
-          :dialogMessage="errorMessage"
-        ></dialog-app>
-        <success-app
-          v-if="show"
-          :isOk="show"
-          message="Başarı ile düzenlendi ."
-        ></success-app>
-
-        <v-row>
-          <v-col>
-            <v-sheet class="p-2" min-height="70vh" rounded="lg">
-              <div class="w-100 d-flex-col">
-                <v-avatar color="grey lighten-2" size="128"></v-avatar>
-
-                <v-form
-                  class="w-50 d-flex-col mt-10"
-                  ref="form"
-                  lazy-validation
-                  outlined
-                  @submit.prevent="registerUser"
-                >
-                  <v-text-field
-                    class="w-50"
-                    v-model="firstName"
-                    :counter="10"
-                    label="First Name"
-                    required
-                  ></v-text-field>
-
-                  <v-text-field
-                    class="w-50"
-                    v-model="lastName"
-                    :counter="10"
-                    label="First Name"
-                    required
-                  ></v-text-field>
-
-                  <v-textarea v-model="description" color="teal" class="w-50">
-                    <template v-slot:label>
-                      <div>Description <small>(optional)</small></div>
-                    </template>
-                  </v-textarea>
-
-                  <v-text-field
-                    class="input-group--focused w-50"
-                    label="Hourly Rate"
-                    v-model="hourlyRate"
-                    required
-                  ></v-text-field>
-                  <div class="selectBoxs d-flex">
-                    <v-switch
-                      class="mr-4"
-                      v-model="tags"
-                      label="Front End"
-                      color="primary"
-                      value="frontend"
-                      hide-details
-                    ></v-switch>
-                    <v-switch
-                      class="mr-4"
-                      v-model="tags"
-                      label="Back End "
-                      color="primary"
-                      value="backend"
-                      hide-details
-                    ></v-switch>
-                    <v-switch
-                      v-model="tags"
-                      label="Career"
-                      class="mr-4"
-                      color="primary"
-                      value="career"
-                      hide-details
-                    ></v-switch>
-                  </div>
-                  <div class="d-flex mt-10">
-                    <v-btn color="success" class="mr-4" @click="handleSave">
-                      Tamamla
-                    </v-btn>
-                  </div>
-                </v-form>
-              </div>
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  <el-main>
+    <el-form align="center">
+      <el-form-item label="First Name">
+        <el-input v-model="firstName"></el-input>
+      </el-form-item>
+      <el-form-item label="Last Name">
+        <el-input v-model="lastName"></el-input>
+      </el-form-item>
+      <el-form-item label="Description">
+        <el-input type="textarea" v-model="description"></el-input>
+      </el-form-item>
+      <el-form-item label="Hourly Rate">
+        <el-input v-model="hourlyRate"></el-input>
+      </el-form-item>
+      <el-form-item label="Tags">
+        <el-checkbox-group v-model="tags">
+          <el-checkbox label="frontend"></el-checkbox>
+          <el-checkbox label="backend"></el-checkbox>
+          <el-checkbox label="career"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-button type="primary" @click="handleSave">Complete</el-button>
+    </el-form>
+  </el-main>
 </template>
 
 <script>
 import { db } from "../firebase";
-import Success from "../components/Success";
+import router from "../router/index";
+// import Success from "../components/Success";
 
 export default {
   props: ["userDetail"],
   components: {
-    "success-app": Success,
+    // "success-app": Success,
   },
   data() {
     return {
@@ -127,6 +59,7 @@ export default {
         })
         .then(() => {
           console.log("successful");
+          router.push("/profile");
           this.show = true;
         })
         .catch((error) => {
@@ -142,26 +75,16 @@ export default {
 </script>
 
 <style scoped>
-.mt-5 {
-  margin-top: 5rem;
+.el-main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.detail-wrapper {
-  max-width: 720px;
+.el-form {
+  width: 80vmin;
 }
-
-.request {
-  float: right;
-  padding: 1rem;
-  border: 1px solid #cacaca;
-  border-radius: 8px;
-  margin: 1rem;
-  background-color: #488fef;
-  color: white;
-  cursor: pointer;
-}
-.p-half {
-  padding: 0.5rem;
-  width: auto;
+.el-form > .el-button {
+  width: 100%;
 }
 </style>
