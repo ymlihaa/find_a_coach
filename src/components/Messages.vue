5087e90b0e4df4c79a1d63760a8e93f982075c9e
ymlihaa <ymlihaa@gmail.com>
2021-04-13 16:44:28 +0300

    message comp converted to element UI .

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,74 +1,29 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app>
-      <v-sheet color="grey lighten-4" class="pa-4">
-        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
-
-        <div>{{ email }}</div>
-      </v-sheet>
-
-      <v-divider></v-divider>
-
-      <v-list>
-        <v-list-item v-for="[icon, text] in links" :key="icon" link>
-          <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-container class="py-8 px-6" fluid>
-      <v-row>
-        <v-col cols="12">
-          <v-card>
-            <v-subheader>Today</v-subheader>
-
-            <v-list two-line>
-              <template v-for="(message, index) in allMessages">
-                <v-list-item :key="index">
-                  <v-list-item-avatar color="grey darken-1">
-                  </v-list-item-avatar>
-
-                  <v-list-item-content>
-                    <v-list-item-title>{{ message.mail }} </v-list-item-title>
-
-                    <v-list-item-subtitle>
-                      {{ message.msg }}
-                    </v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-
-                <v-divider
-                  v-if="n !== 6"
-                  :key="`divider-${index}`"
-                  inset
-                ></v-divider>
-              </template>
-            </v-list>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app>
+  <el-container>
+    <el-aside width="40vmin">
+      {{ email }}
+      <el-divider></el-divider>
+    </el-aside>
+    <el-main>
+      <el-row v-for="(message, index) in allMessages" :key="index">
+        <el-col>
+          <el-card>
+            <strong>
+              {{ message.mail }}
+            </strong>
+            <el-divider></el-divider>
+            <el-main>{{ message.msg }}</el-main>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
 import { db } from "../firebase";
 export default {
   data: () => ({
-    cards: ["Today", "Yesterday"],
-    drawer: null,
-    links: [
-      ["mdi-inbox-arrow-down", "Inbox"],
-      ["mdi-send", "Send"],
-      ["mdi-delete", "Trash"],
-      ["mdi-alert-octagon", "Spam"],
-    ],
     allMessages: [],
     email: "",
   }),
@@ -83,3 +38,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.el-card {
+  margin: 0.5rem;
+}
+</style>
