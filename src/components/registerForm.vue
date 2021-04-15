481c2ade5030b4b9cae08567bcc6cd63147efd6a
ymlihaa <ymlihaa@gmail.com>
2021-04-15 21:22:15 +0300

    registerForm component form validate added .

--- src/components/registerForm.vue
+++ src/components/registerForm.vue
@@ -1,28 +1,30 @@
 <template>
   <el-container type="flex">
     <el-main align="center">
-      <el-form>
-        <el-form-item label="First Name">
-          <el-input v-model="firstName" type="text"></el-input>
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+        <el-form-item label="First Name" prop="firstName">
+          <el-input v-model="ruleForm.firstName" type="text"></el-input>
         </el-form-item>
-        <el-form-item label="Last Name">
-          <el-input type="text" v-model="lastName"></el-input>
+        <el-form-item label="Last Name" prop="lastName">
+          <el-input type="text" v-model="ruleForm.lastName"></el-input>
         </el-form-item>
-        <el-form-item label="Description">
-          <el-input type="textarea" v-model="description"></el-input>
+        <el-form-item label="Description" prop="description">
+          <el-input type="textarea" v-model="ruleForm.description"></el-input>
         </el-form-item>
-        <el-form-item label="Hourly Rate">
-          <el-input type="text" v-model="hourlyRate"></el-input>
+        <el-form-item label="Hourly Rate" prop="hourlyRate">
+          <el-input type="text" v-model="ruleForm.hourlyRate"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-checkbox-group v-model="tags">
+          <el-checkbox-group v-model="ruleForm.tags">
             <el-checkbox label="frontend "></el-checkbox>
             <el-checkbox label="backend"></el-checkbox>
             <el-checkbox label="career"></el-checkbox>
           </el-checkbox-group>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="handleSave">Save</el-button>
+          <el-button type="primary" @click="handleSave('ruleForm')"
+            >Save</el-button
+          >
         </el-form-item>
       </el-form>
     </el-main>
@@ -39,32 +41,80 @@ import router from "../router/index";
 export default {
   data() {
     return {
-      firstName: "",
-      lastName: "",
-      description: "",
-      hourlyRate: "",
-      tags: [],
-      val1: "",
-      val2: "",
-      val3: "",
+      ruleForm: {
+        firstName: "",
+        lastName: "",
+        description: "",
+        hourlyRate: "",
+        tags: [],
+        val1: "",
+        val2: "",
+        val3: "",
+      },
+      rules: {
+        firstName: [
+          {
+            required: true,
+            message: "Please enter your first name .",
+            trigger: "blur",
+          },
+        ],
+        lastName: [
+          {
+            required: true,
+            message: "Please enter your last name .",
+            trigger: "blur",
+          },
+        ],
+        description: [
+          {
+            required: true,
+            message: "Plase enter your description .",
+            trigger: "blur",
+          },
+        ],
+        hourlyRate: [
+          {
+            required: true,
+            message: "Please enter your hourly price ",
+            trigger: "blur",
+          },
+        ],
+      },
     };
   },
   methods: {
-    handleSave() {
-      db.collection("users")
-        .doc(this.$store.state.uid)
-        .set({
-          firstName: this.firstName,
-          lastName: this.lastName,
-          description: this.description,
-          hourlyRate: this.hourlyRate,
-          tags: this.tags,
-        })
-        .then(() => {
-          console.log("successful");
-          router.push("/");
-        })
-        .catch((err) => console.log(err.message));
+    handleSave(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          db.collection("users")
+            .doc(this.$store.state.uid)
+            .set({
+              firstName: this.ruleForm.firstName,
+              lastName: this.ruleForm.lastName,
+              description: this.ruleForm.description,
+              hourlyRate: this.ruleForm.hourlyRate,
+              tags: this.ruleForm.tags,
+            })
+            .then(() => {
+              this.$notify({
+                title: "Success",
+                message: "Register successfully .",
+                type: "success",
+              });
+              router.push("/");
+            })
+            .catch((err) => {
+              this.$notify.error({
+                title: "Error",
+                message: err.message,
+              });
+            });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
   },
   updated() {
