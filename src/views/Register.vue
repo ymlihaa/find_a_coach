17d6b07463d7be0551446c7eb56344f33d819258
ymlihaa <ymlihaa@gmail.com>
2021-04-15 21:01:07 +0300

    register component form validate added .

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,21 +6,27 @@
           <img src="../assets/login_logo.svg" alt="" />
         </el-col>
         <el-col class="form-container">
-          <el-form align="center" v-if="this.$store.state.isActive === false">
+          <el-form
+            align="center"
+            v-if="this.$store.state.isActive === false"
+            :model="formData"
+            :rules="rules"
+            ref="formData"
+          >
             <el-form-item label="E-mail" prop="email">
               <el-input type="email" v-model="formData.email"></el-input>
             </el-form-item>
-            <el-form-item label="Password">
+            <el-form-item label="Password" prop="password">
               <el-input type="password" v-model="formData.password"></el-input>
             </el-form-item>
-            <el-form-item label="Confirm Password">
+            <el-form-item label="Confirm Password" prop="confirmPassword">
               <el-input
                 type="password"
                 v-model="formData.confirmPassword"
               ></el-input>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="registerUser"
+              <el-button type="primary" @click="registerUser('formData')"
                 >Sing Up</el-button
               >
               <el-button type="danger" @click="reset">Reset</el-button>
@@ -60,40 +66,70 @@ export default {
         password: "",
         confirmPassword: "",
       },
+      rules: {
+        email: [
+          {
+            required: true,
+            message: "Please enter your E-mail adress .",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "Please enter your password .",
+            trigger: "blur",
+          },
+        ],
+        confirmPassword: [
+          {
+            required: true,
+            message: "Please confirm your password .",
+            trigger: "blur",
+          },
+        ],
+      },
     };
   },
   methods: {
-    async registerUser() {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(
-          this.formData.email.trim(),
-          this.formData.password.trim()
-        )
-        .then((userCredential) => {
-          var user = userCredential.user;
-          console.log(user);
-          return db
-            .collection("users")
-            .doc(user.uid)
-            .set({
-              email: user.email,
+    async registerUser(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          firebase
+            .auth()
+            .createUserWithEmailAndPassword(
+              this.formData.email.trim(),
+              this.formData.password.trim()
+            )
+            .then((userCredential) => {
+              var user = userCredential.user;
+              console.log(user);
+              return db
+                .collection("users")
+                .doc(user.uid)
+                .set({
+                  email: user.email,
+                })
+                .then(() => {
+                  this.$store.commit("updateLoginAndRegister", {
+                    uid: user.uid,
+                    isActive: true,
+                  });
+                  console.log("uid : ", this.$store.state.uid);
+                });
             })
-            .then(() => {
-              this.$store.commit("updateLoginAndRegister", {
-                uid: user.uid,
-                isActive: true,
+            .catch((error) => {
+              this.$notify({
+                title: "Warning",
+                message: error.message,
+                type: "warning",
               });
-              console.log("uid : ", this.$store.state.uid);
             });
-        })
-        .catch((error) => {
-          this.$notify({
-            title: "Warning",
-            message: error.message,
-            type: "warning",
-          });
-        });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
     reset() {
       this.formData.email = "";
