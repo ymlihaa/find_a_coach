a3d0ddc309d75b2c9ce60627c82c083dc6965d6d
ymlihaa <ymlihaa@gmail.com>
2021-04-17 01:36:11 +0300

    code refactoring .

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import { firestorePlugin } from "vuefire";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import { store } from "./store/store";
-import VueEllipseProgress from "vue-ellipse-progress";
-import vuetify from "./plugins/vuetify";
 import nApp from "./App.vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
@@ -18,10 +16,8 @@ axios.defaults.headers["Accept-Language"] = lang;
 
 Vue.use(ElementUI);
 Vue.use(AnimatedVue);
-
 Vue.use(firestorePlugin);
 Vue.use(VueAxios, axios);
-Vue.use(VueEllipseProgress);
 Vue.config.productionTip = false;
 
 new Vue({
@@ -29,6 +25,5 @@ new Vue({
   store,
   AnimatedVue,
   router,
-  vuetify,
   render: (h) => h(nApp),
 }).$mount("#app");
